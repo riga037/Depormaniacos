26c42c2bc3a29c7260c5b94b8f6961955f6ce84b
Djordje261 <95472719+Djordje261@users.noreply.github.com>
2023-05-16 17:07:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const httpServer = createServer(app);
 
 app.use(express.static("public"));
 
-const io = new Server(httpServer); 
+const io = new Server(httpServer);
 
 var usuarios = [];
 var puntos = [];
@@ -22,9 +22,9 @@ var loop;
 
 io.on("connection", (socket) => {
   console.log('Cliente conectado.');
-  
+
   io.emit("usuarios", usuarios);
-  
+
   socket.on("nickname", (data) => {
     if (data === "admin") {
       socket.emit("admin", true);
@@ -33,58 +33,56 @@ io.on("connection", (socket) => {
       io.emit("usuarios", usuarios);
     }
   });
-  
-  
-socket.on("score", (data) => {
-  const usuario = puntos.find(punto => punto.nom === data.name);
-  if (usuario) usuario.punts += parseInt(data.puntos);
-});
 
-  
-  
-socket.on("register", data => {
-  [preguntes, loop] = [require(`./public/${data}.json`), setInterval(updateTimers, 1000)];
-});
 
-socket.on("restart", (data) => {
-  clearInterval(loop);
-  usuarios = [];
-  puntos = [];
-  time1 = 10;
-  time2 = 15;
-  quest = -1;
-  io.emit("reestablish", true);
-});
+  socket.on("score", (data) => {
+    const usuario = puntos.find((punto) => punto.nom === data.name);
+    if (usuario) usuario.punts += parseInt(data.puntos);
+  });
 
-function updateTimers() {
-  if (time1 > 0) {
-    puntos = usuarios.map(usuario => ({ nom: usuario, punts: 0 }));
-    time1--;
-    io.sockets.emit('time1', time1);
-  } else {
-    io.emit("start", true);
-    if (quest === -1) {
-      puntos.sort((a, b) => b.punts - a.punts);
-      io.sockets.emit('puntos', puntos);
-      io.sockets.emit('pregunta', preguntes[++quest]);
-    } else if (time2 > 0) {
-      time2--;
-      io.sockets.emit('time2', time2);
-    } else if (time2 === 0) {
-      if (quest < preguntes.length - 1) {
-        time2 = 10;
+
+  socket.on("register", (data) => {
+    [preguntes, loop] = [require(`./public/${data}.json`), setInterval(updateTimers, 1000)];
+  });
+
+  socket.on("restart", (data) => {
+    clearInterval(loop);
+    usuarios = [];
+    puntos = [];
+    time1 = 10;
+    time2 = 15;
+    quest = -1;
+    io.emit("reestablish", true);
+  });
+
+  function updateTimers() {
+    if (time1 > 0) {
+      puntos = usuarios.map((usuario) => ({ nom: usuario, punts: 0 }));
+      time1--;
+      io.sockets.emit("time1", time1);
+    } else {
+      io.emit("start", true);
+      if (quest === -1) {
         puntos.sort((a, b) => b.punts - a.punts);
-        io.sockets.emit('puntos', puntos);
-        io.sockets.emit('pregunta', preguntes[++quest]);
-      } else {
-        io.sockets.emit('puntos', puntos);
-        io.emit("end", true);
+        io.sockets.emit("puntos", puntos);
+        io.sockets.emit("pregunta", preguntes[++quest]);
+      } else if (time2 > 0) {
+        time2--;
+        io.sockets.emit("time2", time2);
+      } else if (time2 === 0) {
+        if (quest < preguntes.length - 1) {
+          time2 = 10;
+          puntos.sort((a, b) => b.punts - a.punts);
+          io.sockets.emit("puntos", puntos);
+          io.sockets.emit("pregunta", preguntes[++quest]);
+        } else {
+          io.sockets.emit("puntos", puntos);
+          io.emit("end", true);
+        }
       }
     }
   }
-}
-
-
+});
 
 httpServer.listen(5005, () => {
   console.log(`Server listening at http://localhost:5005`);
